38db2bc2a560e32f491d57fffff1cc5d5029be00
SP Dev Group <manager@spdevgroup.com>
2025-12-02 07:39:42 +1100

    Title update

--- index.py
+++ index.py
@@ -44,7 +44,7 @@ contents = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <head>
 <meta content="text/html; charset=ISO-8859-1"
 http-equiv="content-type">
-<title>Python Webbrowser</title>
+<title>News Site 1</title>
 </head>
 <body>
 <table>
